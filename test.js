9a92bca764c8023eae4376855cb65d88256580e9
Jeffrey Gilbert <visualjeff@linux.com>
2015-03-18 13:46:38 -0700

    generators added

--- test.js
+++ test.js
@@ -728,6 +728,9 @@ describe('template strings', function(){
   })
 })
 
+/*
+ * NOT currently supported?
+ *
 describe('RegExp "u" & "y" flags', function(){
   it('"u" flag', function(){
     var re = new RegExp('\\w');
@@ -737,6 +740,7 @@ describe('RegExp "u" & "y" flags', function(){
     return (assert.strictEqual(re.exec('xy')[0],'x') && assert.strictEqual(re2.exec('xy')[0],'y'));
   })
 })
+*/
 
 describe('RegExp "u" & "y" flags', function(){
   it('"y" flag', function(){
@@ -979,6 +983,9 @@ describe('Class', function(){
   })
 })
 
+/*
+ * NOT CURRENTLY SUPPORTED
+ *
 describe('Class', function(){
   it('extends expression', function(){
     var B;
@@ -988,6 +995,7 @@ describe('Class', function(){
       B.prototype.isPrototypeOf(C.prototype));
   })
 })
+*/
 
 describe('Class', function(){
   it('extends null', function(){
@@ -1075,6 +1083,245 @@ describe('Super', function(){
     return assert.strictEqual(obj.qux(), "barley");
   })
 })
+
+describe('generators', function() {
+  it('basic functionality', function() {
+    function * generator() {
+      yield 5; yield 6;
+    };
+    var iterator = generator();
+    var item = iterator.next();
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('correct this binding', function() {
+    function * generator() {
+      yield this.x; yield this.y;
+    };
+    var iterator = { g: generator, x: 5, y: 6 }.g();
+    var item = iterator.next();
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('sending', function() {
+    var sent;
+    function * generator() {
+      sent = [yield 5, yield 6];
+    };
+    var iterator = generator();
+    iterator.next();
+    iterator.next("foo");
+    iterator.next("bar");
+    return sent[0] === "foo" && sent[1] === "bar";
+  });
+});
+
+
+describe('generators', function() {
+  it('%GeneratorPrototype%', function() {
+    function * generatorFn() {
+      var ownProto = Object.getPrototypeOf(generatorFn());
+      var passed = ownProto === generatorFn.prototype;
+
+      var sharedProto = Object.getPrototypeOf(ownProto);
+      passed &= sharedProto !== Object.prototype && 
+	     sharedProto === Object.getPrototypeOf(function*(){}.prototype) &&
+	    sharedProto.hasOwnProperty('next');
+     return passed; 
+    };
+  });
+});
+
+
+describe('generators', function() {
+  it('%GeneratorPrototype%.throw', function() {
+    var passed = false;
+    function * generator() {
+      try {
+        yield 5, yield 6;
+      } catch(e) {
+	passed = (e === "foo");      
+      }
+    };
+    var iterator = generator();
+    iterator.next();
+    iterator.throw("foo");
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('%GeneratorPrototype%.return', function() {
+    function * generator() {
+      yield 5, yield 6;
+    };
+    var iterator = generator();
+    var item = iterator.next();
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.return("quxquux");
+    passed &= item.value === "quxquux" && item.done === true;
+    item = iterator.next();
+    passed &= item.value === "undefined" && item.done === true;
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('yield operator precedence', function() {
+    var passed;
+    function * generator() {
+      passed = yield 0 ? true : false;
+    };
+    var iterator = generator();
+    iterator.next();
+    iterator.next(true);
+    return passed;
+  });
+});
+
+
+// yield * means to yield and iterable object which yield iterates over.
+//
+describe('generators', function() {
+  it('yield *,arrays', function() {
+    var iterator = function * generator() {
+      yield * [5, 6];
+    }();
+    var item = iterator.next();
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
+
+describe('generators', function() {
+  it('yield *,strings', function() {
+    var iterator = function * generator() {
+      yield * "56";
+    }();
+    var item = iterator.next();
+    var passed = item.value === "5" && item.done === false;
+    item = iterator.next();
+    passed &= item.value === "6" && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('yield *,astral plane strings', function() {
+    var iterator = function * generator() {
+      yield * "🐄🐄";
+    }();
+    var item = iterator.next();
+    var passed = item.value === "🐄" && item.done === false;
+    item = iterator.next();
+    passed &= item.value === "🐄" && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('yield *,generic iterables', function() {
+    var iterator = (function * generator() {
+      yield * __createIterableObject(5,6,7);
+    }());
+    var item = iterator.next();
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 7 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+describe('generators', function() {
+  it('yield *,instances of generic iterables', function() {
+    var iterator = (function * generator() {
+      yield * Object.create(__createIterableObject(5,6,7));
+    }());
+    var item = iterator.next();
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 7 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+describe('generators', function() {
+  it('shorthand generator methods', function() {
+    var o = {
+      * generator() {
+	yield 5, yield 6;	    
+      }
+    }	  
+    var iterator = o.generator();
+    var item = iterator.next()
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
+describe('generators', function() {
+  it('computed shorthand generators', function() {
+    var garply = "generator";
+    var o = {
+      * [garply] () {
+	yield 5, yield 6;	    
+      }
+    }	  
+    var iterator = o.generator();
+    var item = iterator.next()
+    var passed = item.value === 5 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === 6 && item.done === false;
+    item = iterator.next();
+    passed &= item.value === undefined && item.done === true;
+    return passed;
+  });
+});
+
+
 
 function __createIterableObject(a, b, c) {
   if (typeof Symbol === "function" && Symbol.iterator) {
